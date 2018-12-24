67eb293983ea3b00d74ccc2f4bde604f6ae9e39f
Auree Aubert <contact@auree.me>
2018-12-24 19:48:13 +0100

    update

--- src/http/middlewares/auth.js
+++ src/http/middlewares/auth.js
@@ -1,16 +1,18 @@
 const _ = require("lodash");
 
+const ShipSettingsDecryption = require("../../utils/ship-settings-decryption");
 const Client = require("../../client");
 
 module.exports = (req, res, next) => {
   const token = req.headers["ship-token"];
-  const settings = req.headers["ship-settings"];
+  const encryptedSettings = req.headers["ship-settings"];
   const lastCollectedAt = req.headers["ship-last-collected-at"];
 
   if (!token || !settings || !lastCollectedAt) {
     return res.status(401).send({ message: "Unauthorized access" });
   }
 
+  const settings = new ShipSettingsDecryption(settings).decrypt();
   const id = _.first(_.split(Buffer.from(token, "base64").toString(), ":"));
 
   req.client = new Client(token);
