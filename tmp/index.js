1b5554f19899ddfbffb0a7125b5f67cd5d1eb0c0
Auree Aubert <contact@auree.me>
2018-12-24 19:42:22 +0100

    update

--- tmp/index.js
+++
@@ -1,5 +0,0 @@
-const CaptchaSolver = require("../src/captcha-solver");
-
-const captchaSolver = new CaptchaSolver("YXVyZWVhdWJlcnQ6N1YzWmttUzJWZDlGSGhl");
-captchaSolver.solveReCaptcha("hello", "http://www.deathbycaptcha.com/user/login").then(response => console.log(response)).catch(e => { console.log(e); })
-
